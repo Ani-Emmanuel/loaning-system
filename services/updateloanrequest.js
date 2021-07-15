b02182c22cc2fa4750ee14dfcffcb45b95196c4b
Emmanuel Ani <mailsforanih@gmail.com>
2021-07-15 22:02:16 +0100

    service for updating loan

--- services/updateloanrequest.js
+++ services/updateloanrequest.js
@@ -1,10 +1,22 @@
-const Loan = require('../models/wallet');
+const { Wallet } = require('../models');
 const { errorResponse, successResponse } = require('../utils/response');
 
-const UpdateLoanRequest = async ({ _id }, req, res, next) => {
-  try {
-    
-  } catch (error) {
-    next(errorResponse(error.message, 501));
-  }
+//Update loan 
+const UpdateLoanRequest = async (verifiedToken, req, res, next) => {
+	try {
+		const _id = req.params.id;
+		const data = req.body.status;
+		const wallet = await Wallet.findByIdAndUpdate(
+			_id,
+			{
+				$set: { status: data }
+			},
+			{ new: true, runValidators: true }
+		);
+		res.status(200).json(successResponse('Update successful', wallet, 200));
+	} catch (error) {
+		next(error.message);
+	}
 };
+
+module.exports = { UpdateLoanRequest };
