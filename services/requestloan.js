4935a2d233bbceed24e1ae848e05c31ddecc92a9
Emmanuel Ani <mailsforanih@gmail.com>
2021-07-15 22:01:54 +0100

    added service for requesting loan

--- services/requestloan.js
+++ services/requestloan.js
@@ -1,27 +1,36 @@
-const Loan = require('../models/wallet');
+const { Wallet } = require('../models');
 const { errorResponse, successResponse } = require('../utils/response');
+const InterestCalculator = require('../utils/interestcalculator');
+const { RATE } = require('../config');
 
-const RequestLoan = async ({ _id }, req, res, next) => {
+//Request loan service
+const RequestLoan = async ({ _id: userId, role }, req, res, next) => {
 	try {
-		const { amount } = req.body;
-		if (!amount) {
+		if (role === 'user') {
+			const { amount, duration } = req.body;
+			if (!amount) {
+				res
+					.status(401)
+					.json(errorResponse('Invalid request amount is required', 401));
+			}
+			const payload = {};
+			payload.amount = amount;
+			payload.duration = duration;
+			payload.total = InterestCalculator(amount, RATE, duration);
+			payload.userId = userId;
+			const wallet = new Wallet(payload);
+			const savedWallet = await wallet.save();
 			res
-				.status(401)
-				.json(errorResponse('Invalid request amount is required', 401));
+				.status(201)
+				.json(
+					successResponse('request submitted successfully', savedWallet, 201)
+				);
+		} else {
+			res.status(401).json(errorResponse('Sorry you should not be here', 401));
 		}
-		const payload = {};
-		payload.amount = amount;
-		payload.userId = _id;
-		const wallet = new Loan(payload);
-		const savedWallet = await wallet.save();
-		res
-			.status(201)
-			.json(
-				successResponse('request submitted successfully', savedWallet, 201)
-			);
 	} catch (error) {
-		next(errorResponse(error.message, 501));
+		next(error);
 	}
 };
 
-module.exports = RequestLoan;
+module.exports = { RequestLoan };
