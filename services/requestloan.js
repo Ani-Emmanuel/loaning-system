87ab2743185d3492ad9791d1ccdbcfc9ccb181ee
Emmanuel Ani <mailsforanih@gmail.com>
2021-07-21 09:33:44 +0100

    refactored and add checks for any pending or unpaid loan before taking another loan

--- services/requestloan.js
+++ services/requestloan.js
@@ -1,4 +1,4 @@
-const { Wallet } = require('../models');
+const { Loan, User } = require('../models');
 const { errorResponse, successResponse } = require('../utils/response');
 const InterestCalculator = require('../utils/interestcalculator');
 const { RATE } = require('../config');
@@ -7,23 +7,53 @@ const { RATE } = require('../config');
 const RequestLoan = async ({ _id: userId, role }, req, res, next) => {
 	try {
 		if (role === 'user') {
+			const user = await User.findOne({ _id: userId });
+			const loans = await Loan.find({ userId });
+
+			//checks if user exists
+			if (user === null) {
+				return res.status(404).json({ message: 'User does not exist' });
+			}
+
+			//checks if you are eligible to take a loan
+			if (loans.length) {
+				for (let x = 0; x < loans.length; x++) {
+					//checks if there is a loan that has been disbursed but not paid
+					if (loans[x].status === 'DISBURSED' && !loans[x].paid) {
+						return res.status(401).json({
+							message: `you are not authorized to take another loan until you fully pay ${loans[x].total}`
+						});
+					}
+					
+					//checks if there is a loan that has not been disbursed
+					if (
+						loans[x].status !== 'DISBURSED' ||
+						loans[x].status !== 'REJECTED'
+					) {
+						return res.status(401).json({
+							message: `you have a loan in process`
+						});
+					}
+				}
+			}
+
 			const { amount, duration } = req.body;
 			if (!amount) {
 				res
-					.status(401)
-					.json(errorResponse('Invalid request amount is required', 401));
+					.status(400)
+					.json(errorResponse('Invalid request amount is required', 400));
 			}
 			const payload = {};
 			payload.amount = amount;
 			payload.duration = duration;
 			payload.total = InterestCalculator(amount, RATE, duration);
 			payload.userId = userId;
-			const wallet = new Wallet(payload);
-			const savedWallet = await wallet.save();
+			const loan = new Loan(payload);
+			const savedLoan = await loan.save();
 			res
 				.status(201)
 				.json(
-					successResponse('request submitted successfully', savedWallet, 201)
+					successResponse('request submitted successfully', savedLoan, 201)
 				);
 		} else {
 			res.status(401).json(errorResponse('Sorry you should not be here', 401));
