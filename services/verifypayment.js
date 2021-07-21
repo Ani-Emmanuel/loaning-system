d8964d1af987d715a2b4d6082318cb84acf6f99a
Emmanuel Ani <mailsforanih@gmail.com>
2021-07-21 09:43:28 +0100

    refactored and add extra checks

--- services/verifypayment.js
+++ services/verifypayment.js
@@ -1,4 +1,4 @@
-const { Wallet, User } = require('../models');
+const { Loan, User } = require('../models');
 const { errorResponse, successResponse } = require('../utils/response');
 const { verifyPayment } = require('../utils/axios');
 const { PAYSTACKSECRET } = require('../config');
@@ -8,56 +8,87 @@ const verifypayment = async ({ _id: userId, role }, req, res, next) => {
 	try {
 		const _id = req.params.id;
 
-		//Makin sure its the User that took then loan verified it
+		//Making sure its the User that took then loan verifies it
 		if (role === 'user') {
-			const wallet = await Wallet.findOne({ _id, userId });
-			if (wallet === null) {
-				res.status(404).json(errorResponse('There is no Loan with ID', 404));
+			const loan = await Loan.findOne({ _id, userId });
+			if (loan === null) {
+				return res
+					.status(404)
+					.json(errorResponse('There is no Loan with the ID', 404));
 			}
 
-			if (wallet.ref === null) {
-				res.status(400).json(errorResponse('Loan has not been paid', 400));
+			if (loan.ref === null) {
+				return res
+					.status(400)
+					.json(errorResponse('Loan has not been paid', 400));
 			}
 
-			const reference = wallet.ref;
+			const reference = loan.ref;
 			const data = await verifyPayment(reference, PAYSTACKSECRET);
 
 			if (data.status === 'success') {
-				await Wallet.findOneAndUpdate(
+				let totalAmountPaid = Number(loan.refunds) + Number(data.amount / 100);
+				let payment = await Loan.findOneAndUpdate(
 					{ _id, userId },
-					{ $set: { paid: true } }
+					{ $set: { refunds: totalAmountPaid, ref: null } },
+					{ new: true }
 				);
-				return res.status(200).json({ message: 'Load has been paid!!' });
+
+				if (payment.refunds >= loan.total) {
+					await Loan.findOneAndUpdate(
+						{ _id, userId },
+						{ $set: { paid: true } }
+					);
+					return res.status(200).json({ message: 'Loan fully paid' });
+				}
+				return res.status(200).json({
+					message: `you have made ${data.amount / 100} of ${loan.total}`
+				});
 			} else {
 				return res
-					.status(401)
-					.json(errorResponse('Loan has not been paid', 401));
+					.status(400)
+					.json(errorResponse('Loan has not been paid', 400));
 			}
 		}
 
 		//This is for the Admin
-		const wallet = await Wallet.findOne({ _id, userId });
-		if (wallet === null) {
-			res.status(404).json(errorResponse('There is no Loan with ID', 404));
+		const loan = await Loan.findOne({ _id, userId });
+		if (loan === null) {
+			return res
+				.status(404)
+				.json(errorResponse('There is no Loan with ID', 404));
 		}
 
-		if (wallet.ref === null) {
-			res.status(400).json(errorResponse('Loan has not been paid', 400));
+		if (loan.ref === null) {
+			return res.status(400).json(errorResponse('Loan has not been paid', 400));
 		}
 
-		const reference = wallet.ref;
+		const reference = loan.ref;
 		const data = await verifyPayment(reference, PAYSTACKSECRET);
 		if (data.status === 'success') {
-			await Wallet.findOneAndUpdate(
+			let totalAmountPaid = Number(loan.refunds) + Number(data.amount / 100);
+			let payment = await Loan.findOneAndUpdate(
 				{ _id, email: data.customer.email },
-				{ $set: { paid: true } }
+				{ $set: { refunds: totalAmountPaid, ref: null } },
+				{ new: true }
 			);
-			return res.status(200).json({ message: 'Load has been paid!!' });
+
+			if (payment.refunds >= loan.total) {
+				await Loan.findOneAndUpdate(
+					{ _id, email: data.customer.email },
+					{ $set: { paid: true } }
+				);
+				return res.status(200).json({ message: 'Loan fully paid' });
+			}
+
+			return res.status(200).json({
+				message: `you have made ${data.amount / 100} of ${loan.total}`
+			});
 		} else {
-			return res.status(401).json(errorResponse('Loan has not been paid', 401));
+			return res.status(400).json(errorResponse('Loan has not been paid', 400));
 		}
 	} catch (error) {
-		next(error);
+		next(errorResponse(error.message, 500));
 	}
 };
 
